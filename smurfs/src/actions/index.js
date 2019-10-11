9800d65e3de8a1fdb735d7bdbd65042b97d03712
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-11 13:14:38 -0400

    Styled the app

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,7 +4,9 @@ import axios from 'axios';
 export const START_FETCHING = 'START_FETCHING';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAILURE = 'FETCH_FAILURE';
-
+export const START_POST = 'START_POST';
+export const POST_SUCESS = 'POST_SUCESS';
+export const POST_FAILURE = 'POST_FAILURE'
 
 // action creator
 export const fetchSmurfs = () => dispatch => {
@@ -19,3 +21,14 @@ export const fetchSmurfs = () => dispatch => {
     })
     .catch(err => dispatch({ type: FETCH_FAILURE, payload: err.response }));
 };
+
+export const addSmurfs = (smurf) => dispatch => {
+  dispatch({ type: START_POST});
+  axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => {
+      console.log("POST Request Response: ", res)
+      dispatch({type: POST_SUCESS, payload: res.data.smurf })
+    })
+    .catch(err => dispatch({type: POST_FAILURE, payload: err.payload }))
+}
